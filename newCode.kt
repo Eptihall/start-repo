9fabfbbe32149e3337f06d3f92182b3720e909d6
Eptihall <eptihalabdelgafar@gmail.com>
2023-09-07 05:31:31 +0300

    new code kotlin

---
+++ newCode.kt
@@ -0,0 +1,12 @@
+fun addNumber( a:Int , b:Int):Int{
+return a+b
+}
+fu main(){
+[B[A[A[C[Cn[B[A main(){
+ val c=addNumber(56,-23)
+
+}[A[A
+
+[A[A[A
+[A[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D
+[B[B[B
